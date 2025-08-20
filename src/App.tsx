04be06d00b1e0e377e775c5ea803e2e01f431fec
Lorem Ipsum <lorem@ipsum.com>
2025-08-20 13:27:34 -0400

    Prettier formatting. Search value now managed by React.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import './App.css'
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
 
 type Story = {
-  objectID: number,
-  url: string,
-  title: string,
-  author: string,
-  num_comments: number,
-  points: number,
+  objectID: number;
+  url: string;
+  title: string;
+  author: string;
+  num_comments: number;
+  points: number;
 };
 
 const stories: Story[] = [
   {
-    title: 'React',
-    url: 'https://react.dev/',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://react.dev/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
@@ -30,13 +30,13 @@ const stories: Story[] = [
 ];
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  
-  const handleSearch = (event : React.ChangeEvent<HTMLInputElement>) => {
+  const [searchTerm, setSearchTerm] = useState("React");
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredStories = stories.filter(story =>
+  const filteredStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -44,23 +44,29 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
       <List list={filteredStories} />
     </div>
   );
-}
+};
 
 type SearchProps = {
+  search: string;
   onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const Search = (props: SearchProps) => (
   <div>
-    <label htmlFor='search'>Search: </label>
-    <input id='search' type='text' onChange={props.onSearch} />
+    <label htmlFor="search">Search: </label>
+    <input
+      id="search"
+      type="text"
+      value={props.search}
+      onChange={props.onSearch}
+    />
   </div>
 );
 
@@ -69,17 +75,18 @@ type ListProps = {
 };
 
 const List = (props: ListProps) => {
-  return (
-    props.list.length > 0 ? (
-      <ul>
-        {props.list.map((item: Story) => (
-          <Item key={item.objectID} item={item} />
-        ))}
-      </ul>) : (
-        <div><em>No results found</em></div>
-      )
+  return props.list.length > 0 ? (
+    <ul>
+      {props.list.map((item: Story) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  ) : (
+    <div>
+      <em>No results found</em>
+    </div>
   );
-}
+};
 
 type ItemProps = {
   item: Story;
@@ -88,7 +95,9 @@ type ItemProps = {
 const Item = (props: ItemProps) => (
   <li>
     <span>
-      <a href={props.item.url} target='_blank'>{props.item.title}</a>
+      <a href={props.item.url} target="_blank">
+        {props.item.title}
+      </a>
     </span>
     <span> {props.item.author}</span>
     <span> {props.item.num_comments}</span>
@@ -96,4 +105,4 @@ const Item = (props: ItemProps) => (
   </li>
 );
 
-export default App
+export default App;
